e6c30c164f15d51b95d142813d30c47ebc488d1a
khw00185 <ksng0185@naver.com>
2023-12-21 19:54:50 +0900

    sql파일

---
+++ BBS.sql
@@ -0,0 +1,26 @@
+create database BBSS;
+use BBSS;
+
+create table USER(
+userID varchar(20),
+userPassword varchar(20),
+userName varchar(20),
+userGender varchar(20),
+userEmail varchar(60),
+primary key (userID)
+);
+
+INSERT INTO USER value('gildong', '123456', '홍길동', '남자', 'gildong@naver.com');
+INSERT INTO USER value('gilsoon', '654321', '홍길순', '여자', 'gilsoon@naver.com');
+
+select * from user;
+
+create table BBS (
+bbsID INT,
+bbsTitle VARCHAR(50),
+userID VARCHAR(20),
+bbsDate datetime,
+bbsContent VARCHAR(2048),
+bbsAvailable INT,
+primary key (bbsID)
+);
